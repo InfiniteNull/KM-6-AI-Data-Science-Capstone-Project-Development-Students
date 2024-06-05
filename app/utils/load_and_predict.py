0efb6f83ef731a772800e7d236b4faebd40a6dc2
InfiniteNull <108531521+InfiniteNull@users.noreply.github.com>
2024-06-05 14:45:12 +0700

    Create load_and_predict.py

---
+++ app/utils/load_and_predict.py
@@ -0,0 +1,27 @@
+import pickle
+
+# Load the model and vectorizer
+with open('../models/logistic_regression_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open('../models/tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
+    vectorizer = pickle.load(vectorizer_file)
+
+def predict_sentiment(text):
+    text_tfidf = vectorizer.transform([text])
+    prediction = model.predict(text_tfidf)
+    return prediction[0]
+
+def plot_word_cloud(data, sentiment):
+    from wordcloud import WordCloud, STOPWORDS
+    import matplotlib.pyplot as plt
+    import io
+    import base64
+
+    corpus = data[data['Sentiment'] == sentiment]['Text'].str.cat(sep=' ')
+    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white').generate(corpus)
+
+    buffer = io.BytesIO()
+    wordcloud.to_image().save(buffer, format='PNG')
+    img_str = base64.b64encode(buffer.getvalue()).decode()
+    return img_str
